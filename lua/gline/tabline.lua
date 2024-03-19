375e919f8e10c7dd77248a73e957f55720a97136
gremble0 <hermanstornes@gmail.com>
2024-03-19 02:43:17 +0100

    Fix icons and padding, add stylua.toml

--- lua/gline/tabline.lua
+++ lua/gline/tabline.lua
@@ -1,6 +1,6 @@
 -- Define some shortcuts for highlight groups
-local HIGHLIGHT_NORM = "%#TablineNorm#" -- TODO: %#Tabline#
-local HIGHLIGHT_NORM_SEP = "%#TablineNormSep#"
+local HIGHLIGHT_NORM = "%#Tabline#"
+local HIGHLIGHT_NORM_SEP = "%#TablineSep#"
 local HIGHLIGHT_SEL = "%#TablineSel#"
 local HIGHLIGHT_SEL_SEP = "%#TablineSelSep#"
 
@@ -12,24 +12,6 @@ local tabpage_get_active_buf = function(tabpage)
 
   return type(buflist) == "number" and buflist or buflist[winnr]
 end
-
--- ---@param entry string
--- ---@param width integer
--- ---@return string
--- local entry_pad_to_width = function(entry, width)
---   local entry_len = entry:len()
---   return string.rep(" ", entry_len - math.floor(width / 2))
---     .. entry
---     .. string.rep(" ", entry_len - math.ceil(width / 2))
--- end
---
--- ---Either pads or trims the entry string to the given width
--- ---@param entry string
--- ---@param width integer
--- ---@return string
--- local entry_fit_to_width = function(entry, width)
---   return entry_rendered_length(entry) > width and name_trim_to_width(entry, width) or entry_pad_to_width(entry, width)
--- end
 
 ---This class is a mockup of the returntype of vim.fn.gettabinfo()
 ---@class Tab
@@ -47,10 +29,9 @@ end
 ---@return string
 local component_icon = function(tab)
   local tabpage_active_buf = tabpage_get_active_buf(tab.tabnr) -- TODO: fix?
-  local buf_name = vim.fn.bufname(tabpage_active_buf)
   local buf_ft = vim.api.nvim_buf_get_option(tabpage_active_buf, "ft")
   -- TODO: pcall() ?
-  local icon, icon_hl = require("nvim-web-devicons").get_icon(buf_name, buf_ft, { default = true })
+  local icon, icon_hl = require("nvim-web-devicons").get_icon_by_filetype(buf_ft, { default = true })
 
   return "%#" .. icon_hl .. "#" .. icon
 end
@@ -71,7 +52,7 @@ local component_name = function(tab)
   -- TODO: get [No Name] from vim api? i think there is some option to change this
   -- TODO: expand when no name gets set ref: fugitive
 
-  return (tabpage_is_active and HIGHLIGHT_SEL or HIGHLIGHT_NORM) .. name_trim_to_width(name, 22) -- TODO: fix hardcoded 22 and 30
+  return (tabpage_is_active and HIGHLIGHT_SEL or HIGHLIGHT_NORM) .. name_trim_to_width(name, 22) -- TODO: fix hardcoded numbers
 end
 
 ---@param tab Tab
@@ -94,8 +75,8 @@ end
 ---@param entry string
 ---@param width integer
 ---@return string left_padding, string right_padding
-local entry_add_padding = function(entry, width)
-  local total_padding = width - entry_rendered_length(entry) -- TODO: entry:rendered_length() ?
+local entry_pad_to_width = function(entry, width)
+  local total_padding = width - entry_rendered_length(entry) -- TODO: entry:rendered_length() ?, entry:add_component()
 
   return (" "):rep(math.floor(total_padding / 2)), (" "):rep(math.ceil(total_padding / 2) + 1)
 end
@@ -107,10 +88,10 @@ local tabline_make_entry = function(tab)
   local icon = " " .. component_icon(tab)
   local name = " " .. component_name(tab)
   local modified = " " .. component_modified(tab)
-  modified = modified:len() > 1 and (" " .. modified) or ""
+  modified = modified:len() > 1 and modified or ""
 
-  local entry_unpadded = separator .. icon .. name .. modified .. "  "
-  local left_padding, right_padding = entry_add_padding(entry_unpadded, 30) -- TODO: fix hardcoded 30 and 22
+  local entry_unpadded = separator .. icon .. name .. modified
+  local left_padding, right_padding = entry_pad_to_width(entry_unpadded, 26) -- TODO: fix hardcoded numbers
 
   return separator .. left_padding .. icon .. name .. modified .. right_padding
 end
