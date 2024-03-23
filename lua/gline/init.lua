5bff7628affcef6f90dce8a6d69911346a93ac45
gremble0 <hermanstornes@gmail.com>
2024-03-23 06:36:27 +0100

    Fix wrong rebase selection

--- lua/gline/init.lua
+++ lua/gline/init.lua
@@ -2,10 +2,6 @@ local M = {}
 
 ---@class GlineConfig
 local default_config = {
-  colors = {
-    active_bg = "#151515", -- TODO: nil by default
-    inactive_bg = "#000000",
-  },
   entry_width = 22,
   name = {
     enabled = true,
@@ -16,16 +12,18 @@ local default_config = {
     icon = "●",
   },
   ft_icon = {
-    enabled = true, -- if you have web-devicons installed but want it disabled for this plugin, set to false
-    --TODO: colored?
-    --TODO: default_icon
+    enabled = true, -- if you have web-devicons but want it disabled for this plugin, set to false
   },
   separator = {
     enabled = true,
-    active_icon = "▎",
-    inactive_icon = "▏",
-    active_color = "#e1b655",
-    inactive_color = "#282828",
+    selected = {
+      icon = "▎",
+      color = "#e1b655", ---@type string hex color or highlight group
+    },
+    normal = {
+      icon = "▏",
+      color = "#282828", ---@type string hex color or highlight group
+    },
   },
 }
 
@@ -35,7 +33,7 @@ M.config = default_config
 ---@param opts GlineConfig?
 M.setup = function(opts)
   if opts then
-    M.config = vim.tbl_deep_extend("force", default_config, opts)
+    M.config = vim.tbl_extend("force", default_config, opts)
   end
 
   vim.o.tabline = "%!v:lua.require('gline.tabline')()"
